dd2a25e8667186896a75b7b87a58c76a085e4c42
H3L7BOY <109512295+H3L7BOY@users.noreply.github.com>
2025-11-08 10:28:49 +0100

    Refactor next.config.js for module syntax and patterns
    Refactor next.config.js to use CommonJS module syntax and simplify image remote patterns.

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.pexels.com', pathname: '/photos/**' },
+      { protocol: 'https', hostname: 'storage.googleapis.com', pathname: '/workspace-*/image/**' },
+      { protocol: 'https', hostname: 'replicate.delivery', pathname: '/**' },
+    ],
+  },
+};
+
+module.exports = nextConfig;
